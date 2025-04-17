9a5d63f18fb5a19d2db263798dfb8e43d3cddf4b
maximiliano.tudela <m.tudela@alumno.um.edu.ar>
2025-04-17 07:40:22 -0300

    implementar tests para ingreso de números negativos

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -3,7 +3,6 @@ from unittest.mock import patch
 import sys
 import os
 
-# Agregar directorio src al path para importar los módulos
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class TestCalculoNumeros(unittest.TestCase):
@@ -20,6 +19,22 @@ class TestCalculoNumeros(unittest.TestCase):
         from src.calculo_numeros import ingrese_numero
         numero = ingrese_numero()
         self.assertEqual(numero, 12.5)
+
+
+    def test_ingreso_negativo(self, patch_input):
+        """Prueba el caso de ingreso de un número negativo."""
+        from src.calculo_numeros import ingrese_numero
+        from src.exceptions import NumeroDebeSerPositivo
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
+    
+    @patch('builtins.input', return_value='0')
+    def test_ingreso_cero(self, patch_input):
+        """Prueba el caso de ingreso del número cero."""
+        from src.calculo_numeros import ingrese_numero
+        from src.exceptions import NumeroDebeSerPositivo
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
 
 if __name__ == '__main__':
     unittest.main()
